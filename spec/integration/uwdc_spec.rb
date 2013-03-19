fd770e3e6339d369b0e7cf1115e74e3d6b24cf44
Eric Larson <ewlarson@gmail.com>
2013-03-19 16:45:39 -0500

    Add a couple METS parsing specs

--- spec/integration/uwdc_spec.rb
+++ spec/integration/uwdc_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
+require 'uwdc'
 
 describe 'UWDC::Mets' do
   before(:each) do
     @id = '33QOBSVPJLWEM8S'
   end
 
-  it "should return METS" do
-    expect(UWDC::Mets.new(@id)).to be_successful
+  context 'METS' do
+    xml = UWDC::Mets.new(@id).xml
+    it "should return a Nokogiri document" do
+      expect(xml).to be_an_instance_of(Nokogiri::XML::Document)
+    end
+    
+    it 'should have mods' do
+      expect(xml.xpath('//mods')).to be_true
+    end
   end
 end
