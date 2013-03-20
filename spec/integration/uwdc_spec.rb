051450f99c200aecacdd2de7822c667b4dc9f51f
Eric Larson <ewlarson@gmail.com>
2013-03-20 11:02:35 -0500

    Test JSON, Ruby, XML conversions

--- spec/integration/uwdc_spec.rb
+++ spec/integration/uwdc_spec.rb
@@ -1,19 +1,83 @@
 require 'spec_helper'
 require 'uwdc'
 
-describe 'UWDC::Mets' do
+describe UWDC::Mets do
   before(:each) do
     @id = '33QOBSVPJLWEM8S'
+    @mods = UWDC::Mods.new(@id)
+    @origin = UWDC::Origin.new(@id)
+    @rels_ext = UWDC::RelsExt.new(@id)
   end
 
   context 'METS' do
-    xml = UWDC::Mets.new(@id).xml
+    mets = UWDC::Mets.new(@id)
     it "should return a Nokogiri document" do
-      expect(xml).to be_an_instance_of(Nokogiri::XML::Document)
+      expect(mets.nodes).to be_an_instance_of(Nokogiri::XML::Document)
+    end
+    
+    it 'should have mets' do
+      expect(mets.nodes.xpath('//mets')).to be_true
+    end
+  end
+  
+  context 'MODS' do
+    it "should return a Nokogiri node set" do
+      expect(@mods.nodes).to be_an_instance_of(Nokogiri::XML::NodeSet)
     end
     
     it 'should have mods' do
-      expect(xml.xpath('//mods')).to be_true
+      expect(@mods.nodes.xpath('//mods')).to be_true
+    end
+    
+    it 'should export to XML' do
+      expect(Nokogiri::XML(@mods.to_xml)).to be_an_instance_of(Nokogiri::XML::Document)
+    end
+    
+    it 'should export to JSON' do
+      expect(JSON.parse(@mods.to_json)).to be_an_instance_of(Hash)
+      expect(JSON.parse(@mods.to_json).keys).to include('mods')
+    end
+    
+    it 'should export to ruby' do
+      expect(@mods.to_ruby).to be_an_instance_of(Hash)
+    end
+  end
+  
+  context 'ORIGIN' do
+    it "should return a Nokogiri node set" do
+      expect(@origin.nodes).to be_an_instance_of(Nokogiri::XML::NodeSet)
+    end
+    
+    it 'should have origin' do
+      expect(@origin.nodes.xpath('//origin')).to be_true
+    end
+    
+    it 'should export to XML' do
+      expect(Nokogiri::XML(@origin.to_xml)).to be_an_instance_of(Nokogiri::XML::Document)
+    end
+    
+    it 'should export to JSON' do
+      expect(JSON.parse(@origin.to_json)).to be_an_instance_of(Hash)
+      expect(JSON.parse(@origin.to_json).keys).to include('origin')
+    end
+  end
+  
+  context 'RelsExt' do
+    it "should return a Nokogiri node set" do
+      expect(@rels_ext.nodes).to be_an_instance_of(Nokogiri::XML::NodeSet)
+    end
+    
+    it 'should have RDF' do
+      expect(@rels_ext.nodes.xpath('//RDF')).to be_true
+    end
+    
+    it 'should export to XML' do
+      expect(Nokogiri::XML(@rels_ext.to_xml)).to be_an_instance_of(Nokogiri::XML::Document)
+    end
+    
+    it 'should export to JSON' do
+      expect(JSON.parse(@rels_ext.to_json)).to be_an_instance_of(Hash)
+      expect(JSON.parse(@rels_ext.to_json).keys).to include('RDF')
     end
   end
 end
