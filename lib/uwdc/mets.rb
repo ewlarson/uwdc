3ff1431345dfc2d24340f0c4e657608e2fdf7dbb
Eric Larson <ewlarson@gmail.com>
2013-03-20 09:14:36 -0500

    Improve variable name, nodes shall be nodes not xml

--- lib/uwdc/mets.rb
+++ lib/uwdc/mets.rb
@@ -17,37 +17,35 @@ module UWDC
       end
     end
     
-    def xml
-      get
-    end
+    alias :nodes :get
     
     def to_json
-      Hash.from_xml(xml.to_xml).to_json
+      Hash.from_xml(nodes.to_xml).to_json
     end
     
     def to_ruby
-      Hash.from_xml(xml.to_xml)
+      Hash.from_xml(nodes.to_xml)
     end
     
     def to_xml
-      xml.to_xml
+      nodes.to_xml
     end
   end
   
   class Mods < Mets
-    def xml
+    def nodes
       get.xpath("//dmdSec[contains(@ID,'#{@id}')]//mods[1]")
     end
   end
   
   class Origin < Mets
-    def xml
+    def nodes
       get.xpath("//amdSec[contains(@ID,'#{@id}')]//origin[1]")
     end
   end
   
   class RelsExt < Mets
-    def xml
+    def nodes
       get.xpath("//amdSec[contains(@ID,'#{@id}')]//RDF[1]")
     end
   end
