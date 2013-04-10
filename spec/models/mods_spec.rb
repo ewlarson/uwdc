caf224f3e059f922210a0c85915481d3210693b6
Eric Larson <ewlarson@gmail.com>
2013-04-10 11:14:43 -0500

    Add UWDC::Mods.names support and specs

---
+++ spec/models/mods_spec.rb
@@ -0,0 +1,62 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
+require 'uwdc'
+
+# PBO - A life idyl
+def get_pbo_mets
+  @get  = Nokogiri::XML.parse("../fixtures/pbo_mets.xml")
+  @id   = '33QOBSVPJLWEM8S'
+end
+
+# AfricaFocus - Hyena Wrestler with Muzzled Hyena
+def get_af_mets
+  @get  = Nokogiri::XML.parse("../fixtures/af_mets.xml")
+  @id   = 'ECJ6ZXEYWUE7B8W'
+end
+
+def supported_mods_attributes
+  [
+    :titles, 
+    :names,
+    :dates,
+    :forms,
+    :abstracts,
+    :subjects,
+    :subjects_heirarchical_geographic,
+    :access_conditions,
+    :related_items
+  ]
+end
+
+describe UWDC::Mods do
+  
+  context 'PBO' do
+    before(:each) do
+      get_pbo_mets
+      @mods = UWDC::Mods.new(@id)
+    end
+    
+    supported_mods_attributes.each do |method|
+      it { expect(@mods).to respond_to(:"#{method}")}
+    end
+    
+    it "should have an empty names array" do
+      expect(@mods.names).to be_an_instance_of(Array)
+      expect(@mods.names.empty?).to be_true
+    end
+  end
+  
+  context 'AfricaFocus' do
+    before(:each) do
+      get_af_mets
+      @mods = UWDC::Mods.new(@id)
+    end
+    
+    it "should have names with roles" do
+      expect(@mods.names).to be_an_instance_of(Array)
+      expect(@mods.names.empty?).to be_false
+      expect(@mods.names.first).to respond_to(:name)
+      expect(@mods.names.first).to respond_to(:role)
+    end
+  end
+end
