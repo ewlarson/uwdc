e507ebe15c051e17b42ef827d26da00adf10a55a
Eric Larson <ewlarson@gmail.com>
2013-04-15 12:25:20 -0500

    Remove sample UWDC items from Mods specs

--- spec/models/mods_spec.rb
+++ spec/models/mods_spec.rb
@@ -2,24 +2,6 @@
 require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
 require 'uwdc'
 
-# AfricaFocus - Hyena Wrestler with Muzzled Hyena
-def get_africa_focus_mets
-  @get  = Nokogiri::XML.parse("../fixtures/africa_focus_mets.xml")
-  @id   = 'ECJ6ZXEYWUE7B8W'
-end
-
-# Artists' Books - A broth for two parents
-def get_artists_books_mets
-  @get  = Nokogiri::XML.parse("../fixtures/artists_books_mets.xml")
-  @id   = 'CXEB4DPJEOYTM8C'
-end
-
-# Publishers' Bindings - A life idyl
-def get_publishers_bindings_mets
-  @get  = Nokogiri::XML.parse("../fixtures/publishers_bindings_mets.xml")
-  @id   = '33QOBSVPJLWEM8S'
-end
-
 def supported_mods_attributes
   [
     :titles, 
