4cca1a7f6ac77bb0aa43a10fac8dab1ec5d5882d
Eric Larson <ewlarson@gmail.com>
2013-04-15 13:02:40 -0500

    Add StructMap specs

---
+++ spec/models/struct_map_spec.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
+require 'uwdc'
+
+describe UWDC::StructMap do
+  
+  context 'PBO' do
+    before(:each) do
+      get_publishers_bindings_mets
+      @struct_map = UWDC::StructMap.new(@id)
+    end
+    
+    it "should have structure" do
+      expect(@struct_map.structure).to be_true
+      expect(@struct_map.structure).to be_an_instance_of(Array)
+    end
+  end
+end
