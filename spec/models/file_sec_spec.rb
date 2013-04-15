98346a02b939db9aedc4251ff2c49fe0bb1712b1
Eric Larson <ewlarson@gmail.com>
2013-04-15 12:54:36 -0500

    Add FileSec specs

---
+++ spec/models/file_sec_spec.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
+require 'uwdc'
+
+describe UWDC::FileSec do
+  
+  context 'PBO' do
+    before(:each) do
+      get_publishers_bindings_mets
+      @file_sec = UWDC::FileSec.new(@id)
+    end
+    
+    it "should have files" do
+      expect(@file_sec.files).to be_true
+      expect(@file_sec.files).to be_an_instance_of(Array)
+    end
+  end
+end
