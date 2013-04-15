36fbef04729e3af52e90934ad1b4c16d5174c55f
Eric Larson <ewlarson@gmail.com>
2013-04-15 12:24:58 -0500

    Add models method to RelsExt and specs

--- lib/uwdc/rels_ext.rb
+++ lib/uwdc/rels_ext.rb
@@ -3,5 +3,12 @@ module UWDC
     def nodes
       @get.xpath("//amdSec[contains(@ID,'#{@id}')]//RDF[1]")
     end
+    
+    def models
+      nodes.xpath("//amdSec[contains(@ID,'RELS-EXT')]").inject({}) do |result, node|
+        result["#{node.attribute('ID')}"] = pick_attribute(node.xpath(".//hasModel"), 'resource')
+        result
+      end
+    end
   end
 end
---
+++ spec/models/rels_ext_spec.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
+require 'uwdc'
+
+describe UWDC::RelsExt do
+  
+  context 'PBO' do
+    before(:each) do
+      get_publishers_bindings_mets
+      @rels_ext = UWDC::RelsExt.new(@id)
+    end
+    
+    it "should have models" do
+      expect(@rels_ext.models).to be_true
+      expect(@rels_ext.models).to be_an_instance_of(Hash)
+    end
+  end
+end
