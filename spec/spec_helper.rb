69d428a976f4e835b69b336caea4d0a2332b5811
Eric Larson <ewlarson@gmail.com>
2013-04-15 12:23:53 -0500

    Add sample UWDC items to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,21 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.order = 'random'
 end
+
+# AfricaFocus - Hyena Wrestler with Muzzled Hyena
+def get_africa_focus_mets
+  @get  = Nokogiri::XML.parse("../fixtures/africa_focus_mets.xml")
+  @id   = 'ECJ6ZXEYWUE7B8W'
+end
+
+# Artists' Books - A broth for two parents
+def get_artists_books_mets
+  @get  = Nokogiri::XML.parse("../fixtures/artists_books_mets.xml")
+  @id   = 'CXEB4DPJEOYTM8C'
+end
+
+# Publishers' Bindings - A life idyl
+def get_publishers_bindings_mets
+  @get  = Nokogiri::XML.parse("../fixtures/publishers_bindings_mets.xml")
+  @id   = '33QOBSVPJLWEM8S'
+end
