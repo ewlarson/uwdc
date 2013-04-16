f1ef462652e90a5496c35b0232f2bc2236139910
Eric Larson <ewlarson@gmail.com>
2013-04-16 15:50:46 -0500

    Add a few notes regarding identifier blues

--- lib/uwdc/dublin_core.rb
+++ lib/uwdc/dublin_core.rb
@@ -21,6 +21,11 @@ module UWDC
       clean_nodes(children.xpath(".//title[1]"))[0]
     end
     
+    # @FIXME: UWDC identifiers are all jacked up
+    # - mets[@OBJID] => 1711.dl:FJIOAPU6Z7UKR8E
+    # - dmdSec[@ID] => x1711-dl_FJIOAPU6Z7UKR8E
+    # - mods/identifier[@type='handle'] => 1711.dl/FJIOAPU6Z7UKR8E
+    # - mods/recordInfo/recordIdentifier => 1711.dl:FJIOAPU6Z7UKR8E.BIB0ˇ
     def fix_id(id)
       "x#{id.gsub('.','-').gsub(':','_')}"
     end
